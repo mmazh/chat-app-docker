43c9313547c9795211db0b8beb8a52e2651234ac
madeeha <m4deeh4@gmail.com>
2023-12-28 19:42:50 -0500

    Add backend and frontend for simple bookshelf app

---
+++ backend/main.py
@@ -0,0 +1,28 @@
+# to activate venv use - .\\flask_venv\Scripts\activate
+
+from bson import ObjectId
+from flask import Flask, render_template, request
+from pymongo import MongoClient
+
+app = Flask(__name__)
+app.secret_key = b'supersecretkey'
+client = MongoClient('localhost', 27017)
+db = client.BookShelf
+shelf = db.bookShelf
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        operation = request.form['submitButton']
+        if operation == "Add Book":
+            title = request.form['title']
+            author = request.form['author']
+            year = request.form['year']
+            if title and author and year:
+                shelf.insert_one({"title" : title, "author" : author, "year" : year})
+        elif operation == "Delete":
+            bookId = request.form['bookId']
+            shelf.delete_one({"_id": ObjectId(bookId)})
+    currentShelf = shelf.find()
+    return render_template('bookShelf.html', books=currentShelf)
